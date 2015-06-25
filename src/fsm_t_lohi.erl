e5183aa5294a920376647e7830b4919756da76c1
Veronika Kebkal <veronika.kebkal@evologics.de>
2015-06-25 16:16:48 +0200

    Refactroing: fsm_t_lohi : Surround operators and commas with spaces.

--- src/fsm_t_lohi.erl
+++ src/fsm_t_lohi.erl
@@ -122,28 +122,26 @@ handle_event(MM, SM, Term) ->
     {timeout, Event} ->
       ?INFO(?ID, "timeout ~140p~n", [Event]),
       case Event of
-        answer_timeout ->
-          SM;
+        answer_timeout -> SM;
         {backoff_timeout, Msg} when State =:= backoff_state ->
           init_ct(SM),
-          fsm:run_event(MM, SM#sm{event= backoff_end}, {send_tone, Msg});
-        {backoff_timeout, _} ->
-          SM;
+          fsm:run_event(MM, SM#sm{event = backoff_end}, {send_tone, Msg});
+        {backoff_timeout, _} -> SM;
         {send_tone, Msg} ->
-          fsm:run_event(MM, SM#sm{event=send_tone}, {send_tone, Msg});
+          fsm:run_event(MM, SM#sm{event = send_tone}, {send_tone, Msg});
         {cr_end, Msg} ->
           ?TRACE(?ID, "CT ~p~n", [get_ct(SM)]),
           SM1 = fsm:clear_timeout(SM, {send_tone, Msg}),
-          case get_ct(SM1) of
-            0 ->
-              fsm:run_event(MM, SM1#sm{event= no_ct}, 	{});
-            _ ->
-              R = CR_Time * random:uniform(),
-              SM2 = fsm:set_timeout(SM1#sm{event = eps}, {ms, 2 * R}, {backoff_timeout, Msg}),
-              fsm:run_event(MM, SM2#sm{event= ct_exist}, 	{})
+          Ct = get_ct(SM1),
+          if Ct =:= 0 ->
+               fsm:run_event(MM, SM1#sm{event = no_ct}, 	{});
+             true ->
+               R = CR_Time * random:uniform(),
+               SM2 = fsm:set_timeout(SM1#sm{event = eps}, {ms, 2 * R}, {backoff_timeout, Msg}),
+               fsm:run_event(MM, SM2#sm{event = ct_exist}, 	{})
           end;
         _ ->
-          fsm:run_event(MM, SM#sm{event=Event}, {})
+          fsm:run_event(MM, SM#sm{event = Event}, {})
       end;
     {connected} ->
       ?INFO(?ID, "connected ~n", []),
@@ -152,18 +150,18 @@ handle_event(MM, SM, Term) ->
       fsm:send_at_command(SM, {at, binary_to_list(Msg), ""});
     {rcv_ul, {command, C}} ->
       fsm:send_at_command(SM, {at, binary_to_list(C), ""});
-    {rcv_ul, {at,_,_,_,_}} ->
+    {rcv_ul, {at, _, _, _, _}} ->
       fsm:cast(SM, alh, {send, {sync, {error, <<"WRONG FORMAT">>} } }),
       SM;
-    {rcv_ul, Msg={at,_PID,_,_,_,_}} ->
+    {rcv_ul, Msg={at, _PID, _, _, _, _}} ->
       nl_mac_hf:insertETS(SM, data_to_sent, {send_tone, Msg}),
       case State of
-        idle -> fsm:run_event(MM, SM#sm{event=transmit_ct}, {send_tone, Msg});
+        idle -> fsm:run_event(MM, SM#sm{event = transmit_ct}, {send_tone, Msg});
         _ -> fsm:cast(SM, alh,  {send, {sync, "OK"} }), SM
       end;
-    {async,_,{recvims,_,_,_,_,_,_,_,_,_}} ->
-      SM;
-    T={async, PID, Tuple={recvim,_,_,_,_,_,_,_,_,_}} ->
+    {async, _, {recvims, _, _, _, _, _, _, _, _, _}} -> SM;
+    T =
+    {async, PID, Tuple = {recvim, _, _, _, _, _, _, _, _, _}} ->
       [H |_] = tuple_to_list(Tuple),
       BPid=
       case PID of
@@ -172,31 +170,31 @@ handle_event(MM, SM, Term) ->
       [SMN, {Flag, STuple}] = parse_ll_msg(SM, T),
       fsm:cast(SMN, alh, {send, {async, list_to_tuple([H | [BPid| tuple_to_list(STuple) ]] )} }),
       case Flag of
-        nothing ->
-          SMN;
+        nothing -> SMN;
         tone ->
-          SMN1 = fsm:set_timeout(SMN#sm{event = eps}, {ms, 3 * CR_Time}, end_of_frame), % if tone received and got no data
+          % if tone received and got no data
+          SMN1 = fsm:set_timeout(SMN#sm{event = eps}, {ms, 3 * CR_Time}, end_of_frame),
           if State =:= cr -> increase_ct(SMN1);
              true -> nothing
           end,
-          fsm:run_event(MM, SMN1#sm{event=rcv_ct}, {});
+          fsm:run_event(MM, SMN1#sm{event = rcv_ct}, {});
         data when State =:= blocking_state ->
           R = CR_Time * random:uniform(),
           fsm:set_timeout(SMN#sm{event = eps}, {ms, CR_Time + R}, end_of_frame);
         data ->
-          fsm:run_event(MM, SMN#sm{event=rcv_data}, {})
+          fsm:run_event(MM, SMN#sm{event = rcv_data}, {})
       end;
     {async, Tuple} ->
       fsm:cast(SM, alh, {send, {async, Tuple} }),
       SMN = fsm:set_timeout(SM#sm{event = eps}, {ms, CR_Time}, end_of_frame),
       case Tuple of
-        {sendstart,_,_,_,_} -> fsm:run_event(MM, SMN#sm{event=rcv_ct},{});
-        {sendend,_,_,_,_}   -> fsm:run_event(MM, SM#sm{event=end_of_frame},{});
-        {recvstart} 	    -> fsm:run_event(MM, SMN#sm{event=rcv_ct},{});
-        {recvend,_,_,_,_}   -> fsm:run_event(MM, SM#sm{event=end_of_frame},{});
+        {sendstart, _, _, _, _} -> fsm:run_event(MM, SMN#sm{event = rcv_ct}, {});
+        {sendend, _, _, _, _} -> fsm:run_event(MM, SM#sm{event = end_of_frame}, {});
+        {recvstart}	-> fsm:run_event(MM, SMN#sm{event=rcv_ct},{});
+        {recvend, _, _, _, _} -> fsm:run_event(MM, SM#sm{event = end_of_frame}, {});
         _ -> SM
       end;
-    {sync, _Req,Answer} ->
+    {sync, _Req, Answer} ->
       fsm:cast(SM, alh, {send, {sync, Answer} }),
       SM;
     UUg ->
@@ -214,11 +212,9 @@ handle_idle(_MM, SM, Term) ->
   case SM#sm.event of
     internal -> init_mac(SM), SM#sm{event = eps};
     _ ->
-      case T = nl_mac_hf:readETS(SM, data_to_sent) of
-        not_inside ->
-          SM#sm{event = eps};
-        _ ->
-          SM#sm{event = transmit_ct, event_params = T}
+      T = nl_mac_hf:readETS(SM, data_to_sent),
+      if T =:= not_inside -> SM#sm{event = eps};
+         true -> SM#sm{event = transmit_ct, event_params = T}
       end
   end.
 
@@ -240,8 +236,7 @@ handle_cr(_MM, SMP, Term) ->
          true ->
            fsm:set_timeout(SM1#sm{event = eps}, {ms, nl_mac_hf:readETS(SM, cr_time)}, {cr_end, Msg})
       end;
-    _ ->
-      SM#sm{event = eps}
+    _ -> SM#sm{event = eps}
   end.
 
 handle_transmit_data(_MM, SM, Term) ->
@@ -253,8 +248,7 @@ handle_transmit_data(_MM, SM, Term) ->
       CR_Time = nl_mac_hf:readETS(SM, cr_time),
       R = CR_Time * random:uniform(),
       fsm:set_timeout(SM#sm{event = eps}, {ms, CR_Time + R}, dp_ends);
-    _ ->
-      SM#sm{event = eps}
+    _ -> SM#sm{event = eps}
   end.
 
 handle_final(_MM, SM, Term) ->
@@ -272,26 +266,25 @@ parse_ll_msg(SM, Tuple) ->
   case Tuple of
     {async, _PID, Msg} ->
       process_async(SM, Msg);
-    _ ->
-      [SM, nothing]
+    _ -> [SM, nothing]
   end.
 
 process_async(SM, Msg) ->
   case Msg of
-    T={recvim,_,_,_,_,_,_,_,_,_} ->
+    T={recvim, _, _, _, _, _, _, _, _, _} ->
       process_recv(SM, T);
-    _ ->
-      [SM, nothing]
+    _ -> [SM, nothing]
   end.
 
 process_recv(SM, T) ->
-  {recvim,Len,P1,P2,P3,P4,P5,P6,P7,Payl} = T,
-  case re:run(Payl,"([^,]*),(.*)",[dotall,{capture,[1,2],binary}]) of
-    {match, [BFlag,Data]} ->
+  {recvim, Len, P1, P2, P3, P4, P5, P6, P7, Payl} = T,
+  case re:run(Payl,"([^,]*),(.*)",[dotall, {capture, [1, 2], binary}]) of
+    {match, [BFlag, Data]} ->
       Flag = nl_mac_hf:num2flag(BFlag, mac),
+      ShortTuple = {Len - 2, P1, P2, P3, P4, P5, P6, P7, Data},
       case Flag of
-        tone -> [SM, {tone, {Len-2,P1,P2,P3,P4,P5,P6,P7,Data}}];
-        data -> [SM, {data, {Len-2,P1,P2,P3,P4,P5,P6,P7,Data}}]
+        tone -> [SM, {tone, ShortTuple}];
+        data -> [SM, {data, ShortTuple}]
       end;
     nomatch -> [SM, nothing]
   end.
