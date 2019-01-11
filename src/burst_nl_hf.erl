aa779e8aa5f0d572dabe73cdeac30a522b321fe0
Veronika Kebkal <veronika.kebkal@evologics.de>
2019-01-11 16:27:55 +0100

    NL burst: add basic send, recv, without relay

---
+++ src/burst_nl_hf.erl
@@ -0,0 +1,283 @@
+%% Copyright (c) 2015, Veronika Kebkal <veronika.kebkal@evologics.de>
+%%                     Oleksiy Kebkal <lesha@evologics.de>
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions
+%% are met:
+%% 1. Redistributions of source code must retain the above copyright
+%%    notice, this list of conditions and the following disclaimer.
+%% 2. Redistributions in binary form must reproduce the above copyright
+%%    notice, this list of conditions and the following disclaimer in the
+%%    documentation and/or other materials provided with the distribution.
+%% 3. The name of the author may not be used to endorse or promote products
+%%    derived from this software without specific prior written permission.
+%%
+%% Alternatively, this software may be distributed under the terms of the
+%% GNU General Public License ("GPL") version 2 as published by the Free
+%% Software Foundation.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+%% IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+%% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+%% IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+%% NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+%% THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+-module(burst_nl_hf). % additional network layer helper functions for burst data
+-compile({parse_transform, pipeline}).
+
+-import(lists, [filter/2, foldl/3, map/2, member/2]).
+
+-include("fsm.hrl").
+-include("nl.hrl").
+
+-export([create_nl_burst_header/2, extract_nl_burst_header/2]).
+-export([geta/1, getv/2, create_default/0, replace/3]).
+-export([check_routing_existance/1]).
+-export([burst_len/1, increase_local_pc/2]).
+-export([get_packets/1, pop_delivered/2, failed_pc/3]).
+-export([bind_pc/3]).
+
+%----------------------------- Get from tuple ----------------------------------
+geta(Tuple) ->
+  getv([flag, id, id_local, src, dst, len, whole_len, payload], Tuple).
+getv(_, empty) -> nothing;
+getv(Types, Tuple) when is_list(Types) ->
+  lists:foldr(fun(Type, L) -> [getv(Type, Tuple) | L] end, [], Types);
+getv(Type, Tuple) when is_atom(Type) ->
+  PT =
+  case Tuple of {_Retries, T} -> T; _-> Tuple end,
+  {Flag, PkgID, PkgIDLocal, Src, Dst, Len, Whole_len, Payload} = PT,
+
+  Structure =
+  [{flag, Flag}, {id, PkgID}, {id_local, PkgIDLocal}, {src, Src}, {dst, Dst},
+   {len, Len}, {whole_len, Whole_len}, {payload, Payload}
+  ],
+  Map = maps:from_list(Structure),
+  {ok, Val} = maps:find(Type, Map),
+  Val.
+
+create_default() ->
+  {data, unknown, 0, 0, 0, 0, 0, <<"">>}.
+
+replace(Types, Values, Tuple) when is_list(Types) ->
+  replace_helper(Types, Values, Tuple);
+replace(Type, Value, Tuple) when is_atom(Type) ->
+  {Flag, PkgID, PkgIDLocal, Src, Dst, Len, Whole_len, Payload} = Tuple,
+  Structure =
+  [{flag, Flag}, {id, PkgID}, {id_local, PkgIDLocal}, {src, Src}, {dst, Dst},
+   {len, Len}, {whole_len, Whole_len}, {payload, Payload}
+  ],
+  list_to_tuple(lists:foldr(
+  fun({X, _V}, L) when X == Type -> [Value | L];
+     ({_X, V}, L) ->  [V | L]
+  end, [], Structure)).
+
+replace_helper([], _, Tuple) -> Tuple;
+replace_helper([Type | Types], [Value | Values], Tuple) ->
+  replace_helper(Types, Values, replace(Type, Value, Tuple)).
+
+%-------------------- Encodeing / Decodinf data --------------------------------
+create_nl_burst_header(SM, T) ->
+  ?TRACE(?ID, "create_payload_nl_burst_header ~p~n", [T]),
+  [Flag, PkgID,LPkgID,  Src, Dst, Len, Whole_len, Payload] =
+    getv([flag, id, id_local, src, dst, len, whole_len, payload], T),
+
+  Flag_Num = ?FLAG2NUM(Flag),
+  Max = share:get(SM, max_burst_len),
+  C_Bits_PkgID = nl_hf:count_flag_bits(?PKG_ID_MAX),
+  C_Bits_Addr = nl_hf:count_flag_bits(?ADDRESS_MAX),
+  C_Bits_Len = nl_hf:count_flag_bits(Max),
+  C_Bits_Flag = nl_hf:count_flag_bits(?FLAG_MAX),
+
+  B_PkgID = <<PkgID:C_Bits_PkgID>>,
+  B_LPkgID = <<LPkgID:C_Bits_PkgID>>,
+  B_Len_Burst = <<Len:C_Bits_Len>>,
+  B_Whole_Len_Burst = <<Whole_len:C_Bits_Len>>,
+  B_Src = <<Src:C_Bits_Addr>>,
+  B_Dst = <<Dst:C_Bits_Addr>>,
+  B_Flag = <<Flag_Num:C_Bits_Flag>>,
+
+  Tmp_Data = << B_Flag/bitstring, B_PkgID/bitstring, B_LPkgID/bitstring,
+                B_Src/bitstring,
+                B_Dst/bitstring, B_Len_Burst/bitstring,
+                B_Whole_Len_Burst/bitstring, Payload/binary>>,
+
+  Is_binary = nl_hf:check_binary(Tmp_Data),
+  if not Is_binary ->
+    Add = nl_hf:add_bits(Tmp_Data),
+    ?INFO(?ID, "Add ~p~n", [Add]),
+
+    <<B_Flag/bitstring, B_PkgID/bitstring, B_LPkgID/bitstring,
+      B_Src/bitstring, B_Dst/bitstring,
+      B_Len_Burst/bitstring,
+      B_Whole_Len_Burst/bitstring, 0:Add, Payload/binary>>;
+  true ->
+    Tmp_Data
+  end.
+
+extract_nl_burst_header(SM, Payload) ->
+  Max = share:get(SM, max_burst_len),
+  C_Bits_Flag = nl_hf:count_flag_bits(?FLAG_MAX),
+  C_Bits_PkgID = nl_hf:count_flag_bits(?PKG_ID_MAX),
+  C_Bits_Addr = nl_hf:count_flag_bits(?ADDRESS_MAX),
+  C_Bits_Len = nl_hf:count_flag_bits(Max),
+
+  <<Flag:C_Bits_Flag, _PkgID:C_Bits_PkgID, LPkgID:C_Bits_PkgID,
+    Src:C_Bits_Addr, Dst:C_Bits_Addr,
+    Len:C_Bits_Len, Whole_len:C_Bits_Len, _Rest/bitstring>> = Payload,
+
+    Bits_header = C_Bits_Flag + 2 * C_Bits_PkgID + 2 * C_Bits_Addr +
+                  2 * C_Bits_Len,
+    Data = nl_hf:cut_add_bits(Bits_header, Payload),
+    Tuple = create_default(),
+    replace(
+    [flag, id_local, src, dst, len, whole_len, payload],
+    [Flag, LPkgID, Src, Dst, Len, Whole_len, Data], Tuple).
+%--------------------------------- Routing -------------------------------------
+check_routing_existance(SM) ->
+  Routing_table = share:get(SM, routing_table),
+  Local_address = share:get(SM, local_address),
+  Q_data = share:get(SM, nothing, burst_data_buffer, queue:new()),
+  case Routing_table of
+    ?ADDRESS_MAX -> [];
+    _ ->
+      lists:filtermap(
+        fun({?ADDRESS_MAX, ?ADDRESS_MAX, _}) -> false;
+           ({From, _, _}) when From =/= Local_address ->
+              exist_data(Q_data, From);
+           ({_ ,_ ,_}) -> false;
+           (To) -> {true, {default, To}}
+        end, Routing_table)
+  end =/= [].
+
+exist_data(Q, A) -> exist_data(Q, A, 0).
+exist_data({[],[]}, _, 0) -> false;
+exist_data({[],[]}, A, _) -> {true, A};
+exist_data(Q, A, C) ->
+  {{value, Q_Tuple}, Q_Tail} = queue:out(Q),
+  Dst = getv(dst, Q_Tuple),
+  if Dst == A -> exist_data(Q_Tail, A, C + 1);
+    true -> exist_data(Q_Tail, A, C)
+  end.
+
+%---------------------- Burst package handling ---------------------------------
+bind_pc(SM, PC, Packet) ->
+  [LocalPC, Dst, Payload] = getv([id_local, dst, payload], Packet),
+  Q_data = queue:to_list(share:get(SM, nothing, burst_data_buffer, queue:new())),
+  NQ =
+  lists:foldr(
+  fun(X, Q) ->
+    [XPC, XLocalPC, XDst] = getv([id, id_local, dst], X),
+    NT = replace([id, payload], [PC, Payload], X),
+    if XPC == unknown, LocalPC == XLocalPC, Dst == XDst ->
+      queue:in(NT, Q);
+    true ->
+      queue:in(X, Q)
+    end
+  end, queue:new(), Q_data),
+
+  ?TRACE(?ID, "bind pc ~p~n", [NQ]),
+  share:put(SM, burst_data_buffer, NQ).
+
+pop_delivered(SM, PC) ->
+  ?TRACE(?ID, "pop_delivered PC=~p ~n", [PC]),
+
+  Q = share:get(SM, nothing, burst_data_buffer, queue:new()),
+  QL = queue:to_list(Q),
+
+  NL =
+  lists:filtermap(fun(X) ->
+    XPC = getv(id, X),
+    if XPC == PC -> false; true -> {true, X} end
+  end, QL),
+
+  NQ = queue:from_list(NL),
+
+  ?TRACE(?ID, "Pop delivered ~p ~p~n", [QL, NL]),
+  [process_asyncs(__, PC),
+    share:put(__, burst_data_buffer, NQ)
+  ](SM).
+
+failed_pc(SM, PC, Dst) ->
+  QL = queue:to_list(share:get(SM, nothing, burst_data_buffer, queue:new())),
+  NQ =
+  lists:foldr(
+  fun(X, Q) ->
+    [XPC, XLocalPC, XSrc, XDst, XLen, XPayload] =
+      getv([id, id_local, src, dst, len, payload], X),
+    Tuple =
+    replace([id_local, src, dst, len, payload],
+            [XLocalPC, XSrc, XDst, XLen, XPayload],
+    create_default()),
+
+    if XPC == PC, XDst == Dst ->
+      queue:in(Tuple, Q);
+    true ->
+      queue:in(X, Q)
+    end
+  end, queue:new(), QL),
+  ?TRACE(?ID, "Update failed ~p~n", [NQ]),
+  [process_asyncs(__, PC),
+   share:put(__, burst_data_buffer, NQ)
+  ](SM).
+
+get_packets(SM) ->
+  Routing_table = share:get(SM, routing_table),
+  Q_data = share:get(SM, nothing, burst_data_buffer, queue:new()),
+  A = get_first_addr(SM, Routing_table, Q_data),
+  Packets = get_packets(SM, Q_data, A, []),
+  if Packets == [] -> [0, nothing, []];
+    true ->
+      Whole_len = burst_len(Packets),
+      ?TRACE(?ID, "Burst data Whole len ~p~n", [Whole_len]),
+      [H | T] = Packets,
+      [Whole_len, H, T]
+  end.
+
+process_asyncs(SM, PC) ->
+  PCS = share:get(SM, nothing, wait_async_pcs, []),
+  ?INFO(?ID, ">>> delete PC ~w from ~w~n", [PC, PCS]),
+  share:put(SM, wait_async_pcs, lists:delete(PC, PCS)).
+
+get_first_addr(routing, T) -> getv(dst, T).
+get_first_addr(_, _, {[],[]}) -> nothing;
+get_first_addr(SM, Routing_table, Q) ->
+  {{value, Q_Tuple}, Q_Tail} = queue:out(Q),
+  Dst = getv(dst, Q_Tuple),
+  Exist = nl_hf:routing_exist(SM, Dst),
+  if Exist -> get_first_addr(routing, Q_Tuple);
+     true ->  get_first_addr(SM, Routing_table, Q_Tail)
+  end.
+
+get_packets(_, Packets) -> Packets.
+get_packets(_, {[],[]}, _, Packets) -> Packets;
+get_packets(SM, Q, Addr, Packets) ->
+  Max_queue = share:get(SM, max_queue),
+  {{value, Q_Tuple}, Q_Tail} = queue:out(Q),
+  Dst = getv(dst, Q_Tuple),
+
+  Packet_handler =
+  fun (LSM, A, L) when A == Dst, L < Max_queue ->
+        get_packets(LSM, Q_Tail, A, [Q_Tuple | Packets]);
+      (LSM, A, _L) when A == Dst ->
+        get_packets(LSM, [Q_Tuple | Packets]);
+      (LSM, A, _L) ->
+        get_packets(LSM, Q_Tail, A, Packets)
+  end,
+  Packet_handler(SM, Addr, length(Packets)).
+%---------------------------- Burst additional ---------------------------------
+burst_len(P) ->
+    lists:foldr(fun (X, A) -> A + getv(len, X) end, 0, P).
+
+increase_local_pc(SM, Name) ->
+  PC = share:get(SM, Name),
+  if PC > 127 ->
+    share:put(SM, Name, 1);
+  true ->
+    share:put(SM, Name, PC + 1)
+  end.
---
+++ src/fsm_nl_burst.erl
@@ -0,0 +1,379 @@
+%% Copyright (c) 2015, Veronika Kebkal <veronika.kebkal@evologics.de>
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions
+%% are met:
+%% 1. Redistributions of source code must retain the above copyright
+%%    notice, this list of conditions and the following disclaimer.
+%% 2. Redistributions in binary form must reproduce the above copyright
+%%    notice, this list of conditions and the following disclaimer in the
+%%    documentation and/or other materials provided with the distribution.
+%% 3. The name of the author may not be used to endorse or promote products
+%%    derived from this software without specific prior written permission.
+%%
+%% Alternatively, this software may be distributed under the terms of the
+%% GNU General Public License ("GPL") version 2 as published by the Free
+%% Software Foundation.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+%% IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+%% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+%% IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+%% NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+%% THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+-module(fsm_nl_burst).
+-behaviour(fsm).
+-compile({parse_transform, pipeline}).
+
+-include("fsm.hrl").
+-include("nl.hrl").
+
+-export([start_link/1, trans/0, final/0, init_event/0]).
+-export([init/1, handle_event/3, stop/1]).
+-export([handle_recv/3, handle_sensing/3, handle_transmit/3, handle_busy/3]).
+-export([handle_idle/3, handle_alarm/3, handle_final/3]).
+
+-define(TRANS, [
+                {idle,
+                 [{try_transmit, sensing},
+                  {routing_updated, sensing},
+                  {recv_data, recv}
+                 ]},
+
+                {recv,
+                 [{recv_data, recv},
+                  {pick, sensing}
+                 ]},
+
+                {sensing,
+                 [{try_transmit, sensing},
+                  {check_state, sensing},
+                  {initiation_listen, transmit},
+                  {empty, idle},
+                  {no_routing, idle},
+                  {busy_backoff, busy},
+                  {busy_online, busy}
+                 ]},
+
+                {transmit,
+                 [{pick, sensing},
+                  {next_packet, transmit},
+                  {recv_data, recv}
+                 ]},
+
+                {busy,
+                 [{backoff_timeout, sensing},
+                  {check_state, sensing}
+                 ]},
+
+                {alarm,
+                 [{final, alarm}
+                 ]},
+
+                {final, []}
+               ]).
+
+start_link(SM) -> fsm:start_link(SM).
+init(SM)       -> init_nl_burst(SM), SM.
+trans()        -> ?TRANS.
+final()        -> [alarm].
+init_event()   -> eps.
+stop(_SM)      -> ok.
+
+handle_event(MM, SM, Term) ->
+  ?INFO(?ID, "HANDLE EVENT ~p ~p Term:~p~n", [SM#sm.state, SM#sm.event, Term]),
+  Local_address = share:get(SM, local_address),
+  Pid = share:get(SM, pid),
+  case Term of
+    {timeout, answer_timeout} ->
+      fsm:cast(SM, nl_impl, {send, {nl, error, <<"ANSWER TIMEOUT">>}});
+    {timeout, check_state} ->
+      [fsm:maybe_send_at_command(__, {at, "?S", ""}),
+       fsm:set_event(__, check_state),
+       fsm:run_event(MM, __, {})
+      ](SM);
+    {timeout, pc_timeout} ->
+      fsm:maybe_send_at_command(SM, {at, "?PC", ""});
+    {connected} ->
+      SM;
+    {allowed} ->
+      share:put(SM, pid, share:get(SM, {pid, MM}));
+    {denied} ->
+      share:put(SM, pid, nothing);
+    {disconnected, _} ->
+      SM;
+    {nl, get, routing} ->
+      Routing = {nl, routing, nl_hf:routing_to_list(SM)},
+      fsm:cast(SM, nl_impl, {send, Routing});
+    {nl, get, protocol} ->
+      Protocol = share:get(SM, nl_protocol),
+      fsm:cast(SM, nl_impl, {send, {nl, protocol, Protocol}});
+    {nl, get, address} ->
+      fsm:cast(SM, nl_impl, {send, {nl, address, Local_address}});
+    {nl, send, _, _} ->
+      fsm:cast(SM, nl_impl, {send, {nl, send, error}});
+    {nl, send, tolerant, _, _} ->
+      Event_handler =
+      fun (LSM) when LSM#sm.state == idle ->
+            fsm:set_event(LSM, try_transmit);
+          (LSM) ->
+            fsm:set_event(LSM, eps)
+      end,
+      [process_nl_send(__, Local_address, Term),
+       Event_handler(__),
+       fsm:run_event(MM, __, {})
+      ](SM);
+    {nl, send, _, _, _} ->
+      fsm:cast(SM, nl_impl, {send, {nl, send, error}});
+    {sync,"?S", Status} ->
+      [fsm:clear_timeout(__, check_state),
+       fsm:clear_timeout(__, answer_timeout),
+       extract_status(__, Status),
+       fsm:run_event(MM, __, Term)
+      ](SM);
+    {sync,"?PC", PC} ->
+      [fsm:clear_timeout(__, pc_timeout),
+       fsm:clear_timeout(__, answer_timeout),
+       process_pc(__, PC),
+       fsm:run_event(MM, __, {})
+      ](SM);
+    {sync,"*SEND",{error, _}} ->
+      [share:put(__, wait_sync, false),
+       fsm:cast(__, nl_impl, {send, {nl, send, error}}),
+       run_hook_handler(MM, __, Term, error)
+      ](SM);
+    % TODO: check if busy state needed
+    %{sync,"*SEND", {busy, _}} ->
+    %  [fsm:maybe_send_at_command(__, {at, "?S", ""}),
+    %   run_hook_handler(MM, __, Term, eps)
+    %  ](SM);
+    {sync, "*SEND", "OK"} ->
+      [fsm:maybe_send_at_command(__, {at, "?PC", ""}),
+       fsm:set_timeout(__, {s, 1}, pc_timeout),
+       run_hook_handler(MM, __, Term, eps)
+      ](SM);
+    {async, {pid, Pid}, {recv, _, _, Local_address , _,  _,  _,  _,  _, P}} ->
+      [process_data(__, P),
+       fsm:set_event(__, recv_data),
+       fsm:run_event(MM, __, {})
+      ](SM);
+    {async, {delivered, PC, _Src}} ->
+      [burst_nl_hf:pop_delivered(__, PC),
+       fsm:set_event(__, check_pcs(__)),
+       fsm:run_event(MM, __, {})
+      ](SM);
+    {async, {failed, PC, Src}} ->
+      [burst_nl_hf:failed_pc(__, PC, Src),
+       fsm:set_event(__, check_pcs(__)),
+       fsm:run_event(MM, __, {})
+      ](SM);
+    UUg ->
+      ?ERROR(?ID, "~s: unhandled event:~p~n", [?MODULE, UUg]),
+      SM
+  end.
+
+check_pcs(SM) ->
+  PCS = share:get(SM, nothing, wait_async_pcs, []),
+  if PCS == [] -> pick; true -> eps end.
+
+run_hook_handler(MM, SM, Term, Event) ->
+  [fsm:clear_timeout(__, answer_timeout),
+   fsm:set_event(__, Event),
+   fsm:run_event(MM, __, Term)
+  ](SM).
+
+init_nl_burst(SM) ->
+  [share:put(__, burst_data_buffer, queue:new()),
+   share:put(__, local_pc, 1),
+   share:put(__, wait_async_pcs, [])
+  ](SM).
+
+%------------------------------Handle functions---------------------------------
+handle_idle(_MM, SM, Term) ->
+  ?TRACE(?ID, "handle_idle ~120p~n", [Term]),
+  SM#sm{event = eps}.
+
+handle_recv(_MM, SM, Term) ->
+  ?TRACE(?ID, "handle_recv ~120p~n", [Term]),
+  SM#sm{event = eps}.
+
+handle_sensing(_MM, #sm{event = pick} = SM, Term) ->
+  ?TRACE(?ID, "handle_sensing ~120p~n", [Term]),
+  Q = share:get(SM, nothing, burst_data_buffer, queue:new()),
+  Queue_handler =
+  fun (LSM, true)  -> LSM#sm{event = empty};
+      (LSM, false) -> LSM#sm{event = try_transmit}
+  end,
+  Queue_handler(SM, queue:is_empty(Q));
+handle_sensing(_MM, #sm{event = check_state} = SM, _) ->
+  [fsm:maybe_send_at_command(__, {at, "?S", ""}),
+   fsm:set_timeout(__, {s, 1}, check_state),
+   fsm:set_event(__, eps)
+  ](SM);
+handle_sensing(_MM, #sm{event = try_transmit} = SM, _) ->
+  % 1. check if there are messages in the buffer with existing
+  %    routing to dst address
+  % 2. if exist -> check state and try transmit
+  %    if not, go back to idle
+  Routing_handler =
+  fun (LSM, true) -> fsm:set_event(LSM, check_state);
+      (LSM, false) -> fsm:set_event(LSM, no_routing)
+  end,
+  Routing_handler(SM, burst_nl_hf:check_routing_existance(SM));
+handle_sensing(_MM, SM, Term) ->
+  ?TRACE(?ID, "handle_sensing ~120p~n", [Term]),
+  SM#sm{event = eps}.
+
+handle_transmit(_MM, #sm{event = next_packet} = SM, _Term) ->
+  {send_params, {Whole_len, Tuple, Tail}} =
+    nl_hf:find_event_params(SM, send_params),
+
+  Packet_handler =
+  fun(Packets) when Packets == [] -> [nothing, []];
+     (Packets) -> [H | T] = Packets, [H, T]
+  end,
+
+  Transmit_handler =
+  fun(LSM, T, Rest) ->
+      Pid = share:get(LSM, pid),
+      [PC, Dst] = burst_nl_hf:getv([id, dst], T),
+      Data = burst_nl_hf:create_nl_burst_header(LSM, T),
+      Route_Addr = nl_hf:get_routing_address(LSM, Dst),
+
+      AT = {at, {pid, Pid}, "*SEND", Route_Addr, Data},
+      [P, NTail] = Packet_handler(Rest),
+      NT = {send_params, {Whole_len, P, NTail}},
+      PCS = share:get(SM, nothing, wait_async_pcs, []),
+
+      [share:put(__, wait_async_pcs, [PC | PCS]),
+       nl_hf:add_event_params(__, NT),
+       fsm:set_event(__, eps),
+       fsm:maybe_send_at_command(__, AT)
+      ](LSM)
+  end,
+
+  Params_handler =
+  fun(LSM, nothing, []) ->
+        fsm:set_event(LSM, eps);
+      (LSM, T, Rest) ->
+        Transmit_handler(LSM, T, Rest)
+  end,
+
+  ?INFO(?ID, "try transmit ~p Rest ~p~n", [Tuple, Tail]),
+  Params_handler(SM, Tuple, Tail);
+handle_transmit(_MM, #sm{event = initiation_listen} = SM, _Term) ->
+  % 1. get packets from the queue for one dst where routing exist for
+  % 2. get current package counter and bind
+  [Whole_len, Packet, Tail] = burst_nl_hf:get_packets(SM),
+  % TODO: check if Packet is nothing
+  NT = {send_params, {Whole_len, Packet, Tail}},
+  ?TRACE(?ID, "Try to send packet Packet ~120p Tail ~p ~n", [Packet, Tail]),
+  [share:put(__, wait_async_pcs, []),
+   fsm:maybe_send_at_command(__, {at, "?PC", ""}),
+   nl_hf:add_event_params(__, NT),
+   fsm:set_timeout(__, {s, 1}, pc_timeout),
+   fsm:set_event(__, eps)
+  ](SM);
+handle_transmit(_MM, SM, Term) ->
+  ?TRACE(?ID, "handle_transmit ~120p~n", [Term]),
+  SM#sm{event = eps}.
+
+handle_busy(_MM, SM, Term) ->
+  ?TRACE(?ID, "handle_busy ~120p~n", [Term]),
+  SM#sm{event = eps}.
+
+-spec handle_alarm(any(), any(), any()) -> no_return().
+handle_alarm(_MM, SM, _Term) ->
+  exit({alarm, SM#sm.module}).
+
+handle_final(_MM, SM, Term) ->
+  ?TRACE(?ID, "Final ~120p~n", [Term]).
+
+% ------------------------------- Helper functions -----------------------------
+process_nl_send(SM, _, T) ->
+  [push_tolerant_queue(__, T),
+   fsm:cast(__, nl_impl, {send, {nl, send, 0}})
+  ](SM).
+
+push_tolerant_queue(SM, {nl, send, tolerant, Dst, Payload}) ->
+  LocalPC = share:get(SM, local_pc),
+  Len = byte_size(Payload),
+  Src = share:get(SM, local_address),
+
+  Tuple =
+  burst_nl_hf:replace([id_local, src, dst, len, payload],
+                      [LocalPC, Src, Dst, Len, Payload],
+              burst_nl_hf:create_default()),
+
+  Q = share:get(SM, nothing, burst_data_buffer, queue:new()),
+  ?TRACE(?ID, "Add to burst queue ~p~n", [Tuple]),
+
+  [burst_nl_hf:increase_local_pc(__, local_pc),
+   share:put(__, burst_data_buffer, queue:in(Tuple, Q))
+  ](SM).
+
+extract_status(SM, Status) ->
+  Parsed = [string:rstr(X, "INITIATION LISTEN") || X <- string:tokens (Status, "\r\n")],
+  ?TRACE(?ID, "~p~n", [Status]),
+  Listen =
+  lists:filtermap(fun(X) ->
+                    if X == 0 -> false;
+                      true -> {true, true}
+                  end end, Parsed),
+  Status_handler =
+  fun (LSM, [true]) ->
+        fsm:set_event(LSM, initiation_listen);
+      (LSM, _) ->
+        [fsm:set_event(__, busy_online),
+         fsm:clear_timeout(__, check_state),
+         fsm:set_timeout(__, {s, 1}, check_state)
+        ](LSM)
+  end,
+  Status_handler(SM, Listen).
+
+init_pc(SM, PC) ->
+  ?INFO(?ID, "init_pc ~p~n", [PC]),
+  share:put(SM, pc, PC).
+
+process_pc(SM, LPC) ->
+  PC = list_to_integer(LPC),
+  Send_params = nl_hf:find_event_params(SM, send_params),
+  case Send_params of
+    {send_params, {Whole_len, Tuple, Tail}} when Tuple =/= nothing ->
+      ?INFO(?ID, "process_pc for ~p~n", [Tuple]),
+      PC_Tuple = burst_nl_hf:replace(id, PC, Tuple),
+      NT = {send_params, {Whole_len, PC_Tuple, Tail}},
+      [init_pc(__, PC),
+       burst_nl_hf:bind_pc(__, PC, Tuple),
+       nl_hf:add_event_params(__, NT),
+       fsm:set_event(__, next_packet)
+      ](SM);
+    _ ->
+      init_pc(SM, PC)
+  end.
+
+process_data(SM, Data) ->
+  Local_address = share:get(SM, local_address),
+  try
+    Tuple = burst_nl_hf:extract_nl_burst_header(SM, Data),
+    [Src, Dst, Payload] = burst_nl_hf:getv([src, dst, payload], Tuple),
+    if Dst == Local_address ->
+      %TODO: send ack
+      [fsm:cast(__, nl_impl, {send, {nl, recv, Src, Dst, Payload}}),
+       fsm:set_event(__, eps)
+      ](SM);
+    true ->
+      fsm:set_event(SM, eps)
+    end,
+    ?TRACE(?ID, "Received ~p~n", [Tuple]),
+     fsm:set_event(SM, eps)
+  catch error: Reason ->
+    % Got a message not for NL, no NL header
+    ?ERROR(?ID, "~p ~p ~p~n", [?ID, ?LINE, Reason]),
+    fsm:set_event(SM, eps)
+  end.
